a2cd6307c4b22321ec780810f0f5fe6d38c0189e
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-11 18:23:45 +0300

    💚 SEO | 💚 Performance | 💚 Accessibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import htmlBeautifier from "astro-html-beautifier";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-
-  ]
+  site: "https://mohannadotaibi.com",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), robotsTxt(), sitemap(), htmlBeautifier({
+    indent_size: 4,
+    brace_style: "collapse",
+    indent_char: "\t",
+    indent_with_tabs: true,
+    end_with_newline: true,
+    wrap_line_length: 80
+  }), compress()]
 });
